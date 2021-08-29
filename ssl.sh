f155b1c570402b7a632dfa0644a8466e7f0ef18f
happy42779 <emailforgong@gmail.com>
2021-08-29 13:11:06 +0800

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# 1. check if acme.sh is installed
+FILE=~/.acme.sh
+if [ ! -d "$FILE" ]; then
+    cd ~
+    git clone https://github.com/acmesh-official/acme.sh.git
+fi
+# 2. renew
 systemctl stop nginx
-/root/.acme.sh/acme.sh --issue -d www.xxxxxx.xxx --standalone
-/root/.acme.sh/acme.sh --installcert -d www.xxxxxx.xxx --fullchain-file /path that used by nginx/xxxxxx.pem --key-file /path that used by nginx/xxxxxx.key
+~/.acme.sh/acme.sh --issue -d www.xxxxxx.xxx --standalone
+~/.acme.sh/acme.sh --installcert -d www.xxxxxx.xxx --fullchain-file /path that used by nginx/xxxxxx.pem --key-file /path that used by nginx/xxxxxx.key
 systemctl start nginx

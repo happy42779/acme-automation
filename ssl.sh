92b4dfb55e8b4b13c3a9fb35daae61c5296183bd
happy42779 <emailforgong@gmail.com>
2021-08-29 14:39:52 +0800

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-# 1. check if acme.sh is installed
-FILE=~/.acme.sh
-if [ ! -d "$FILE" ]; then
+
+while getopts d:p: flag
+do
+    case "${flag}" in
+        d) DOMAIN=${OPTARG};;
+        p) PATH=${OPTARG};;
+    esac
+done
+
+# validating params
+
+# checking if user provided '/' for the path
+
+FILE=~/.acme.sh/acme.sh
+if [ ! -f "$FILE" ]; then
+    ehco "installing acme"
     cd ~
     git clone https://github.com/acmesh-official/acme.sh.git
+    ./acme.sh/acme.sh   --install
 fi
-# 2. renew
-systemctl stop nginx
-~/.acme.sh/acme.sh --issue -d www.xxxxxx.xxx --standalone
-~/.acme.sh/acme.sh --installcert -d www.xxxxxx.xxx --fullchain-file /path that used by nginx/xxxxxx.pem --key-file /path that used by nginx/xxxxxx.key
-systemctl start nginx
+
+echo "installing denpendencies"
+apt-get install socat curl cron openssl
+~/.acme.sh/acme.sh --issue -d $DOMAIN --standalone
+~/.acme.sh/acme.sh --installcert -d $DOMAIN --fullchain-file $PATH/$DOMAIN.pem --key-file $PATH/$DOMAIN.key --reloadcmd "nginx -s reload"

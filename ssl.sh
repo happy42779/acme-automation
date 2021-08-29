fdaeb58df10b9e20e655d4057b7ce12dc6ea3cc4
happy42779 <emailforgong@gmail.com>
2021-08-29 17:00:05 +0800

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
-while getopts d:p: flag
-do
-    case "${flag}" in
-        d) DOMAIN=${OPTARG};;
-        p) PATH=${OPTARG};;
-    esac
-done
+#while getopts d:p: flag
+#do
+#    case "${flag}" in
+#        d) DOMAIN=${OPTARG};;
+#        p) PATH=${OPTARG};;
+#    esac
+#done
 
 # validating params
 
 # checking if user provided '/' for the path
 
-FILE=~/.acme.sh/acme.sh
-if [ ! -f "$FILE" ]; then
-    ehco "installing acme"
-    cd ~
-    git clone https://github.com/acmesh-official/acme.sh.git
-    echo "installing denpendencies"
-    apt-get install socat curl cron openssl
-    ./acme.sh/acme.sh   --install
-fi
-
-~/.acme.sh/acme.sh --issue -d $DOMAIN --standalone
-~/.acme.sh/acme.sh --installcert -d $DOMAIN --fullchain-file $PATH/$DOMAIN.pem --key-file $PATH/$DOMAIN.key --reloadcmd "nginx -s reload"
+~/.acme.sh/acme.sh --issue -d DOMAIN --standalone --server letsencrypt
+~/.acme.sh/acme.sh --installcert -d DOMAIN --fullchain-file PATH/DOMAIN.pem --key-file PATH/DOMAIN.key --reloadcmd "nginx -s reload"
